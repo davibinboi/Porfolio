6c8296cef8eebb3f36912e375f75a056ebd7d35d
Shrikar Seshadri <rangarajanshrikar@gmail.com>
2025-12-04 23:12:24 -0500

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -78,7 +78,7 @@ function NavBar() {
                 <AiOutlineFundProjectionScreen
                   style={{ marginBottom: "2px" }}
                 />{" "}
-                About Me
+                My Experience
               </Nav.Link>
             </Nav.Item>
 
